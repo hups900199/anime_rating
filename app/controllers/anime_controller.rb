88864505fd5eed1bfd6d88b717d5d4e7cee41bc8
Jia Hong Ye <hups900199@gmail.com>
2022-10-11 17:58:45 -0500

    Update About page and Home page.

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -13,9 +13,9 @@ class AnimeController < ApplicationController
     #debugger.log(wildcard_search)
 
     if search_type != ""
-      @animes = Anime.where("name LIKE ?", wildcard_search).where(type_id: search_type)
+      @animes = Anime.where("name LIKE ?", wildcard_search).where(type_id: search_type).order("rating DESC")
     else
-      @animes = Anime.where("name LIKE ?", wildcard_search)
+      @animes = Anime.where("name LIKE ?", wildcard_search).order("rating DESC")
     end
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @animes = Anime.includes(:type)
+                   .order("rating DESC")
+                   .limit(10)
   end
 end
